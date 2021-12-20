9be20e3ed70cf314e5a96ffe244c4aae2c854c56
nkg29 <noragera@microsoft.com>
2021-12-20 13:30:04 -0800

    Create the celestialObjectController class - make it inherit the ControllerBase class - add Route("") and ApiController attributes to the class - add the ApplicationDbContext field, needing the directive for StarChart.Data - create the constructor that accepts a parameterof ApplicationDbContext (should be public!)

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; // field NOT a property so no get; set; required
+        // this doesn't get exposed outside for others when they create objects of that type
+    
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
