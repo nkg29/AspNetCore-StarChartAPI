fb1c5f5261a325693d462065a2937be1aaafa93b
nkg29 <noragera@microsoft.com>
2021-12-20 14:11:27 -0800

    Create celestialObjectController's Get Action - new method GetById with return type IActionResult with the HttpGet attribute("{id:int}") -- the quotes are important - constructors don't have return types! - to get the celestialObject, call the database to Find the id - then set the Satellites property for this current celestialObject to the CelestialObjects where the orbitalId = this id: REMEMBER TO SET the ToList() applied to the result to get the correct type - then return Ok(celestialObject)

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -8,11 +9,23 @@ namespace StarChart.Controllers
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context; // field NOT a property so no get; set; required
-        // this doesn't get exposed outside for others when they create objects of that type
-    
+                                                        // this doesn't get exposed outside for others when they create objects of that type
+
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id: int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+            celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+            return Ok(celestialObject);
         }
     }
 }
